26a7e0930d1f7a5b21dcbb165c628fe91247aeff
Ronny R. Polloqueri <ronny.polloqueri@gmail.com>
2014-03-17 21:51:29 -0500

    Initial Commit

---
+++ application/views/welcome_message.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Curso de CodeIgniter</title>
+	<style>
+		body{
+			font-family: Arial, "Helveltica";
+		}
+	</style>
+
+</head>
+<body>
+
+<div id="container">
+
+	<div id="body">
+		<h1>Hola Mundo vía Codeigniter</h1>
+	</div>
+
+
+</div>
+
+</body>
+</html>
